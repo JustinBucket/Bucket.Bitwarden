d07690dce232d599560adf97e3668ae55e5cec20
bucket <justinduffmailloux@gmail.com>
2025-11-19 00:47:19 -0500

    Added tests for authentication

--- Testing/TestBitwardenCaller.cs
+++ Testing/TestBitwardenCaller.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Net;
+using System.Threading;
 using Bucket.Bitwarden;
+using Bucket.Bitwarden.Auth;
 using Testing.Helpers;
 
 namespace Testing;
@@ -8,11 +11,32 @@ namespace Testing;
 public class TestBitwardenCaller
 {
     [TestMethod]
-    public void TestUnlock()
+    [ExpectedException(typeof(AggregateException))]
+    public void TestUnlockWithNoAuth()
     {
         var caller = new BitwardenCaller();
         var secrets = Helper.GenerateSecrets();
         var success = caller.UnlockVault(secrets.MasterPassword).Result;
+    }
 
+    [TestMethod]
+    [ExpectedException(typeof(AggregateException))]
+    public void TestUnlockWithExpiredAuth()
+    {
+        var caller = new BitwardenCaller();
+        var secrets = Helper.GenerateSecrets();
+        var authResponse = caller.Authenticate(Helper.GenerateAuthData(secrets)).Result;
+        Thread.Sleep(3_601_000);
+        var success = caller.UnlockVault(secrets.MasterPassword).Result;
+    }
+
+    [TestMethod]
+    public void TestAuthenticate()
+    {
+        var caller = new BitwardenCaller();
+        var secrets = Helper.GenerateSecrets();
+        var authData = new AuthData(secrets.ClientId, secrets.ClientSecret);
+
+        var statusCode = caller.Authenticate(authData).Result;
     }
 }
