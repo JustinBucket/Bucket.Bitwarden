0afb4f76fcc38a1cc0e29a63a07780f10f8cb459
bucket <justinduffmailloux@gmail.com>
2025-12-02 00:34:15 -0500

    Created VaultItem to hold data from RetrieveEntry

---
+++ Bucket.Bitwarden/VaultItem.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Bucket.Bitwarden.Get
+{
+    public class VaultItem
+    {
+        // not sure what this actually looks like
+        // public string[] PasswordHistory { get; set; }
+        public DateTime RevisionDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Object { get; set; }
+        public Guid Id { get; set; }
+        public Guid FolderId { get; set; }
+        public int Type { get; set; }
+        public int Reprompt { get; set; }
+        public string Name { get; set; }    
+        public string Notes { get; set; }
+        [JsonProperty("favorite")]
+        public bool Favourite { get; set; }
+        // have an idea what this is - think it's a <string, string> dictionary
+        // public string[] Fields { get; set; }
+    }
+}
